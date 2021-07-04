d216875f3c10d3ea4b81155db8ed391e0dc43f04
Mohammed Shamsheer <shamsheer142089@gmail.com>
2021-07-05 02:35:54 +0530

    LTCDP-02 Longest Common String

--- src/com/leetcode/easy/four/RomanToInt.java
+++ src/com/leetcode/easy/four/RomanToInt.java
@@ -15,9 +15,9 @@ public class RomanToInt {
 	/**
 	 * @param args
 	 */
-	private final static Map<Character,Integer> romKeyVal;
+	private final static Map<Character, Integer> romKeyVal;
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println(romanToInt("XXV"));
 		System.out.println(romanToInt("III"));
 		System.out.println(romanToInt("IV"));
@@ -25,33 +25,32 @@ public class RomanToInt {
 		System.out.println(romanToInt("LVIII"));
 		System.out.println(romanToInt("MCMXCIV"));
 	}
-	
-	private final static Map<Character,Integer> romKeyVal;
+
 	static {
-		 romKeyVal = new HashMap<>();   
-	     romKeyVal.put('I', 1);
-	     romKeyVal.put('V', 5);
-	     romKeyVal.put('L', 50);
-	     romKeyVal.put('X', 10);
-	     romKeyVal.put('C', 100);
-	     romKeyVal.put('D', 500);
-	     romKeyVal.put('M', 1000);
+		romKeyVal = new HashMap<>();
+		romKeyVal.put('I', 1);
+		romKeyVal.put('V', 5);
+		romKeyVal.put('L', 50);
+		romKeyVal.put('X', 10);
+		romKeyVal.put('C', 100);
+		romKeyVal.put('D', 500);
+		romKeyVal.put('M', 1000);
 	}
-	
+
 	public static int romanToInt(String s) {
-	     int maxIndex = s.length()-1;
-	     int tot=romKeyVal.get(s.charAt(maxIndex));
-	     
-	     for(int idx=0;idx<maxIndex;) {
-	    	 int currentVal=romKeyVal.get(s.charAt(idx));
-	    	 if(currentVal < romKeyVal.get(s.charAt(++idx))) {
-	    		 tot = tot - currentVal; 
-	    	 }else {
-	    		 tot = tot + currentVal;
-	    	 }
-	     }
-	      
-	     return tot;
-	    }
+		int maxIndex = s.length() - 1;
+		int tot = romKeyVal.get(s.charAt(maxIndex));
+
+		for (int idx = 0; idx < maxIndex;) {
+			int currentVal = romKeyVal.get(s.charAt(idx));
+			if (currentVal < romKeyVal.get(s.charAt(++idx))) {
+				tot = tot - currentVal;
+			} else {
+				tot = tot + currentVal;
+			}
+		}
+
+		return tot;
+	}
 
 }
